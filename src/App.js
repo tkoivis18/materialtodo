9f5dadd48f50c6f0f1ced160634eaf088848b98f
tkoivis18 <tobias.koivisto@gmail.com>
2023-03-15 00:03:44 +0200

    added tooltip to delete button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { Tooltip } from '@mui/material';
 
 function App() {
+  const [todo, setTodo] = useState({ description: '', date: '' });
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  };
+
+  const addTodo = () => {
+    setTodos([...todos, todo]);
+  };
+
+  const deleteTodo = (row) => {
+    setTodos(todos.filter((todo, index) => index !== row));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Todolist</Typography>
+        </Toolbar>
+      </AppBar>
+      <Stack
+        direction="row"
+        spacing={2}
+        mt={2}
+        justifyContent="center"
+        alignItems="center"
+      ></Stack>
+      <TextField
+        variant="standard"
+        label="Description"
+        name="description"
+        value={todo.description}
+        onChange={inputChanged}
+      />
+      <TextField
+        variant="standard"
+        label="Date"
+        name="date"
+        value={todo.date}
+        onChange={inputChanged}
+      />
+      <Button variant="outlined" onClick={addTodo}>
+        Add
+      </Button>
+      <table>
+        <tbody>
+          {todos.map((todo, index) => (
+            <tr key={index}>
+              <td>{todo.description}</td>
+              <td>{todo.date}</td>
+              <td>
+                <Tooltip title="Delete">
+                  <IconButton
+                    size="small"
+                    color="error"
+                    onClick={() => deleteTodo(index)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </Tooltip>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
